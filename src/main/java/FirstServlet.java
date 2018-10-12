1e91ec2ba5b2071f352a0bc62c245389f2aef371
Илья <iliyalepa@gmail.com>
2018-10-12 21:24:19 +0300

    Initial commit

---
+++ src/main/java/FirstServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *@Author Ilya Lepa
+ */
+public class FirstServlet extends javax.servlet.http.HttpServlet
+{
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws ServletException, IOException
+    {
+
+    }
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws ServletException, IOException
+    {
+        PrintWriter pw = response.getWriter();
+        pw.println("<html>");
+        pw.println("<h1>Privet MIR!</h1>");
+        pw.println("</html>");
+    }
+}
